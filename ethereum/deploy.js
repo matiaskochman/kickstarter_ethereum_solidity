d6b5e6ba2c6971283f4029da089ca84471b085c1
matias <matiaskochman@gmail.com>
2018-03-19 14:38:10 -0300

    addig next.js framework, react and react-dom

---
+++ ethereum/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider('all evil dwarf ensure wool minor neither frame candy strong camera learn',
+'https://rinkeby.infura.io/p3M4EvN162hvHud2S5Z7');
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attemping to deploy from account',accounts[0])
+
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+  .deploy({ data:compiledFactory.bytecode })
+  .send({gas:'1000000',from:accounts[0]})
+
+  //console.log(compiledFactory.interface);
+  console.log('Contract deployed to: ',result.options.address);
+}
+
+deploy();
