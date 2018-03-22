c39a702af94e816ea017f5a7b1bb01201c44e834
matias <matiaskochman@gmail.com>
2018-03-22 12:47:56 -0300

    going further 2

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,11 +1,13 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json');
-const {metamask_wallet_password} = require('./metamask_wallet_password')
+const {metamask_wallet_password ,rinkeby_infura} = require('./metamask_wallet_password')
 
 //console.log(metamask_wallet_password)
+//console.log(rinkeby_infura)
+
 const provider = new HDWalletProvider(metamask_wallet_password,
-'https://rinkeby.infura.io/p3M4EvN162hvHud2S5Z7');
+rinkeby_infura);
 
 const web3 = new Web3(provider);
 
